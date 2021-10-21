a30a161083417bdd1c2c44d9ff4b7105401abfdb
phoenixrhein <phoenixrhein@xovatec.de>
2021-10-21 23:05:46 +0200

    Add model classes

---
+++ app/Models/LinkGroup.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace de\xovatec\linkify\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class LinkGroup extends Model
+{
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'link_group';
+
+}
